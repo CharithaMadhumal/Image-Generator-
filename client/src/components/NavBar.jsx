daa636880172549fe3dbcdbc4f6e8d6d4ab620a9
CharithaMadhumal <charithamadhumal174@gmail.com>
2024-12-22 02:13:37 +0530

    updated header part

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import { AppContext } from '../context/AppContext'
 
 const NavBar = () => {
 
-    const {user} = useContext(AppContext)
+    const {user,setShowLogin} = useContext(AppContext)
     const navigate = useNavigate()
 
     return (
@@ -39,7 +39,7 @@ const NavBar = () => {
                         :
                         <div className='flex items-center gap-2 sm:gap-5'>
                             <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p>
-                            <button className='bg-zinc-700 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button>
+                            <button onClick={()=>setShowLogin(true)} className='bg-zinc-700 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button>
                         </div>
                 }
             </div>
